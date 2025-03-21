745db0dad4749592d4e575417e51c6900ba60113
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-21 12:40:24 -0500

    base:'https://elesconditemotel.lat/FrontendViteReact/', // Ruta base para producción

--- FrontendViteReact/vite.config.ts
+++ FrontendViteReact/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https:dhurtado9611/React-Vite_NestJS-MySql',
+  base:'https://elesconditemotel.lat/FrontendViteReact/', // Ruta base para producción
   build: {
     chunkSizeWarningLimit: 1000, // Aumentar el límite a 1000 kB
   },
