9793598fea61e5c4c96792af7a32d4197b646b7d
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-21 14:36:48 -0500

    base:'/FrontendViteReact/', // Ruta base para producción

--- FrontendViteReact/vite.config.ts
+++ FrontendViteReact/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://elesconditemotel.lat/FrontendViteReact/', // Ruta base para producción
+  base:'/FrontendViteReact/', // Ruta base para producción
   build: {
     chunkSizeWarningLimit: 1000, // Aumentar el límite a 1000 kB
   },
