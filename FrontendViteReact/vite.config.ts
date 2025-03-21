e3ae0dcfe3f2925652d1b5fc92ce72d7eec224c4
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-21 11:52:12 -0500

    Actualizacion de chunkSizeWarningLimit

--- FrontendViteReact/vite.config.ts
+++ FrontendViteReact/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Aumentar el límite a 1000 kB
+  },
 })
