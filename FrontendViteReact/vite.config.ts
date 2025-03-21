80aa824d85faca98750721eea1ada41ce0b4f129
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-21 12:08:40 -0500

    base:'https:dhurtado9611/React-Vite_NestJS-MySql'

--- FrontendViteReact/vite.config.ts
+++ FrontendViteReact/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https:dhurtado9611/React-Vite_NestJS-MySql',
   build: {
     chunkSizeWarningLimit: 1000, // Aumentar el límite a 1000 kB
   },
