2f3c57599f265a08655dc2497edca84a402acd00
Daniel Mauricio Hurtado Vallejo <dhurtado9611@gmail.com>
2025-03-27 15:12:22 -0500

    update: switch database from MySQL to PostgreSQL and add dotenv for environment variable management

--- BackendNestJS/src/app.module.ts
+++ BackendNestJS/src/app.module.ts
@@ -4,18 +4,21 @@ import { Reserva } from './reservas/reserva.entity';
 import { ReservaService } from './reservas/reserva.service';
 import { ReservaController } from './reservas/reserva.controller';
 import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306, // Asegúrate de que coincida con el puerto de MySQL
-      username: 'dhurtado9611',
-      password: 'Porsche918**', // Si tienes contraseña, ponla aquí
-      database: 'reservas_motel', // Tu base de datos
+      type: 'postgres', // Cambiado a PostgreSQL
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10) || 5432, // Puerto por defecto de PostgreSQL
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Reserva], // Las entidades que vas a utilizar
-      synchronize: true, // Activa la sincronización automática
+      synchronize: true, // Para desarrollo, desactívalo en producción
     }),
     TypeOrmModule.forFeature([Reserva]),
     AuthModule,
