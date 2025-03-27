a145136cc511562595aa2020aeed70c65fb73715
Daniel Mauricio Hurtado Vallejo <dhurtado9611@gmail.com>
2025-03-27 15:52:36 -0500

    update: fix parsing of DB_PORT environment variable in TypeOrmModule configuration

--- BackendNestJS/src/app.module.ts
+++ BackendNestJS/src/app.module.ts
@@ -18,7 +18,7 @@ if (!process.env.DB_HOST || !process.env.DB_PORT || !process.env.DB_USER || !pro
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT!, 10) || 5432,
+      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
