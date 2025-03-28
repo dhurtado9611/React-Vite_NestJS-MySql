7e6a168c0870edfb89ab15a3a5f760c409f34436
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-28 00:03:02 -0500

    base: '/FrontendViteReact/', // Ruta base ajustada a la carpeta del proyecto

--- FrontendViteReact/vite.config.ts
+++ FrontendViteReact/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Usa './' o '/' para asegurar que las rutas estén bien configuradas
+  base: '/FrontendViteReact/', // ✅ Ruta base ajustada a la carpeta del proyecto
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
