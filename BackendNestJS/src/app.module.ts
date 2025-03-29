d846546fa12816d3058d5e4917af1d7091ec3a93
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-29 11:39:28 -0500

    update: actualizar mysql2 a la versión 3.14.0 y ajustar configuración de conexión a la base de datos

--- BackendNestJS/src/app.module.ts
+++ BackendNestJS/src/app.module.ts
@@ -5,22 +5,21 @@ import { ReservaService } from './reservas/reserva.service';
 import { ReservaController } from './reservas/reserva.controller';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './app.service';import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST ?? 'localhost',
-      port: parseInt(process.env.DB_PORT ?? '5432'), // Puerto por defecto para PostgreSQL
-      username: process.env.DB_USER ?? 'default_user',
-      password: process.env.DB_PASSWORD ?? 'default_password',
-      database: process.env.DB_NAME ?? 'default_database',
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Reserva],
       synchronize: process.env.NODE_ENV !== 'production',
-      ssl: {
-        rejectUnauthorized: false,
-      },
     }),    
     TypeOrmModule.forFeature([Reserva]),
     AuthModule,
