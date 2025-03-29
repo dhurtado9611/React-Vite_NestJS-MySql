7ac7be8c342b487a8aed995d6471f42e3c6b2d30
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-29 12:54:05 -0500

    update: mejorar configuración de conexión a la base de datos con valores predeterminados y aumentar el tiempo de espera

--- BackendNestJS/src/app.module.ts
+++ BackendNestJS/src/app.module.ts
@@ -13,14 +13,18 @@ dotenv.config();
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '3306'),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      host: process.env.DB_HOST ?? 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '3306'),
+      username: process.env.DB_USER ?? 'root',
+      password: process.env.DB_PASSWORD ?? 'password',
+      database: process.env.DB_NAME ?? 'test',
       entities: [Reserva],
       synchronize: process.env.NODE_ENV !== 'production',
-    }),    
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      connectTimeout: 30000, // Aumenta el tiempo de espera a 30 segundos
+    }),       
     TypeOrmModule.forFeature([Reserva]),
     AuthModule,
   ],
