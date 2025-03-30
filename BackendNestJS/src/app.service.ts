7779552e7afe10ec1182cea8db263525588b8811
Daniel Mauricio Hurtado Vallejo <dhurtado9611@gmail.com>
2025-03-30 17:31:03 -0500

    update: add console log for root access in AppService

--- BackendNestJS/src/app.service.ts
+++ BackendNestJS/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getRoot(): string {
+    console.log('Backend funcionando - se accedió a la ruta "/"');
     return 'API BackendNestJS funcionando correctamente 🚀';
   }
 }
