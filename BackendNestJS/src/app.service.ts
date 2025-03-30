146aa11f21eeceb362dd6176d2445bcd8f4ed069
Daniel Mauricio Hurtado Vallejo <dhurtado9611@gmail.com>
2025-03-30 16:48:19 -0500

    update: modify API root response message and update VITE_API_URL; remove unused vercel.json file

--- BackendNestJS/src/app.service.ts
+++ BackendNestJS/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getRoot(): string {
-    return 'API funcionando correctamente 🚀';
+    return 'API BackendNestJS funcionando correctamente 🚀';
   }
 }
