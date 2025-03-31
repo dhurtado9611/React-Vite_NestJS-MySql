b91b87a633185e031410d5256694a174ad765780
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-31 13:28:35 -0500

    update: agregar nueva URL permitida en la configuración de CORS

--- BackendNestJS/src/main.ts
+++ BackendNestJS/src/main.ts
@@ -6,12 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://react-vite-nest-js-my-ds74mxdem-danielhurtados-projects.vercel.app',
+    origin: [
+      'https://react-vite-nest-js-my-ds74mxdem-danielhurtados-projects.vercel.app',
+      'https://react-vite-nest-js-my-sql.vercel.app' // 👈 Agrega esta nueva URL también
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
-
+  
   const PORT = process.env.PORT || 3000;
   // IMPORTANTE: Añadir explícitamente '0.0.0.0' para escuchar en todas las interfaces
   await app.listen(PORT, '0.0.0.0');
