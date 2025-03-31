b593eec55ebfa48ef6b3fb149d4695c5b002610b
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-31 10:54:31 -0500

    update: habilitar CORS para el frontend en Vercel y ajustar el puerto de escucha

--- BackendNestJS/src/main.ts
+++ BackendNestJS/src/main.ts
@@ -1,17 +1,19 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configurar CORS para permitir solicitudes desde el dominio del frontend
+  // Habilita CORS para permitir solicitudes desde tu frontend en Vercel
   app.enableCors({
-    origin: 'https://react-vite-nest-js-my-ds74mxdem-danielhurtados-projects.vercel.app', // Permitir solo tu frontend
+    origin: 'https://react-vite-nest-js-my-ds74mxdem-danielhurtados-projects.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
-    credentials: true, // Si usas cookies o autenticación
+    credentials: true,
   });
 
-  await app.listen(3000);
+  // 🚨 Aquí está el cambio importante 👇
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
