6c010d946a54cd9ea4bfb271456c687fbc3c3909
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-31 00:54:33 -0500

    update: cambiar la configuración de TypeORM de MySQL a PostgreSQL y agregar archivo de ejemplo de variables de entorno

--- BackendNestJS/src/app.module.ts
+++ BackendNestJS/src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT ?? '3306', 10),
       username: process.env.DB_USERNAME,
