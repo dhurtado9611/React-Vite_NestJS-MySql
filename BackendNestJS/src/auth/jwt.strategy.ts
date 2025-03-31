ccc0cf70e7081df8ff2850379d2e6b1180386ac3
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-31 12:32:16 -0500

    Agregados logs de debug a JwtStrategy

--- BackendNestJS/src/auth/jwt.strategy.ts
+++ BackendNestJS/src/auth/jwt.strategy.ts
@@ -6,14 +6,17 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
+    console.log('📦 JwtStrategy initialized'); // 👈 LOG DE CONFIRMACIÓN
+
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET')!, // 👈 ¡este es clave!
+      secretOrKey: configService.get<string>('JWT_SECRET')!,
     });
   }
 
   async validate(payload: any) {
+    console.log('✅ TOKEN VALIDADO:', payload); // 👈 LOG DE VALIDACIÓN
     return { id: payload.sub, username: payload.username };
   }
 }
