4a28c2fee3ef1f0a70da96afb644cbf1e9b9641f
Daniel Hurtado <dhurtado9611@gmail.com>
2025-03-31 01:10:38 -0500

    update: ajustar la configuración de TypeORM para usar el puerto predeterminado de PostgreSQL

--- BackendNestJS/src/app.module.ts
+++ BackendNestJS/src/app.module.ts
@@ -13,7 +13,7 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT ?? '3306', 10),
+      port: parseInt(process.env.DB_PORT!, 10) || 5432,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
