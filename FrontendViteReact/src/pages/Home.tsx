d793cbe4c9715f4be1a4557bdd9db139768c8582
Daniel Hurtado <dhurtado9611@gmail.com>
2025-04-01 22:39:58 -0500

    update: agregar un contenedor para el pie de página en la página de inicio

--- FrontendViteReact/src/pages/Home.tsx
+++ FrontendViteReact/src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import Carousel from "../components/Carousel";
 import fondo1 from "../assets/fondo1.jpg";
 import Footer from "../components/Footer";
+import { div } from "framer-motion/client";
 
 const Logo = () => {
   const [isAnimating, setIsAnimating] = useState(true);
@@ -73,7 +74,11 @@ const Home = () => {
       <div className="overflow-hidden w-full flex justify-center items-center z-10">
         <Carousel />
       </div>
+      
+      <div className="row">
       <Footer/>
+      </div>
+     
     </div>
   );
 };
