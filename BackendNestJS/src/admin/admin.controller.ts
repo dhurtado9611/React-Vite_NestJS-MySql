3b1c19c603335bf8ff2c4fcd28d7d39300883135
Daniel Hurtado <dhurtado9611@gmail.com>
2025-04-04 01:41:24 -0500

    update: renombrar métodos de eliminación en AdminController para mayor claridad

--- BackendNestJS/src/admin/admin.controller.ts
+++ BackendNestJS/src/admin/admin.controller.ts
@@ -1,92 +1,99 @@
 import {
-    Controller, Get, Post, Put, Delete, Param, Body, NotFoundException, UseGuards
-  } from '@nestjs/common';
-  import { AdminService } from './admin.service';
-  import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-  import { RolesGuard } from '../auth/roles.guard';
-  import { Roles } from '../auth/roles.decorator';
-  
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
-  @Controller()
-  export class AdminController {
-    constructor(private readonly adminService: AdminService) {}
-  
-    // USERS
-    @Get('users')
-    findAllUsers() {
-      return this.adminService.findAll('users');
-    }
-  
-    @Get('users/:id')
-    findOneUser(@Param('id') id: number) {
-      return this.adminService.findOne('users', id);
-    }
-  
-    @Post('users')
-    createUser(@Body() data: any) {
-      return this.adminService.create('users', data);
-    }
-  
-    @Put('users/:id')
-    updateUser(@Param('id') id: number, @Body() data: any) {
-      return this.adminService.update('users', id, data);
-    }
-  
-    @Delete('users/:id')
-    removeUser(@Param('id') id: number) {
-      return this.adminService.remove('users', id);
-    }
-  
-    // RESERVAS
-    @Get('reservas')
-    findAllReservas() {
-      return this.adminService.findAll('reservas');
-    }
-  
-    @Get('reservas/:id')
-    findOneReserva(@Param('id') id: number) {
-      return this.adminService.findOne('reservas', id);
-    }
-  
-    @Post('reservas')
-    createReserva(@Body() data: any) {
-      return this.adminService.create('reservas', data);
-    }
-  
-    @Put('reservas/:id')
-    updateReserva(@Param('id') id: number, @Body() data: any) {
-      return this.adminService.update('reservas', id, data);
-    }
-  
-    @Delete('reservas/:id')
-    removeReserva(@Param('id') id: number) {
-      return this.adminService.remove('reservas', id);
-    }
-  
-    // CUADRE
-    @Get('cuadre')
-    findAllCuadre() {
-      return this.adminService.findAll('cuadre');
-    }
-  
-    @Get('cuadre/:id')
-    findOneCuadre(@Param('id') id: number) {
-      return this.adminService.findOne('cuadre', id);
-    }
-  
-    @Post('cuadre')
-    createCuadre(@Body() data: any) {
-      return this.adminService.create('cuadre', data);
-    }
-  
-    @Put('cuadre/:id')
-    updateCuadre(@Param('id') id: number, @Body() data: any) {
-      return this.adminService.update('cuadre', id, data);
-    }
-  
-    @Delete('cuadre/:id')
-    removeCuadre(@Param('id') id: number) {
-      return this.adminService.remove('cuadre', id);
-    }
-  }  
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('admin')
+@Controller()
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  // USERS
+  @Get('users')
+  findAllUsers() {
+    return this.adminService.findAll('users');
+  }
+
+  @Get('users/:id')
+  findOneUser(@Param('id') id: number) {
+    return this.adminService.findOne('users', id);
+  }
+
+  @Post('users')
+  createUser(@Body() data: any) {
+    return this.adminService.create('users', data);
+  }
+
+  @Put('users/:id')
+  updateUser(@Param('id') id: number, @Body() data: any) {
+    return this.adminService.update('users', id, data);
+  }
+
+  @Delete('users/:id')
+  deleteUser(@Param('id') id: number) {
+    return this.adminService.remove('users', id);
+  }
+
+  // RESERVAS
+  @Get('reservas')
+  findAllReservas() {
+    return this.adminService.findAll('reservas');
+  }
+
+  @Get('reservas/:id')
+  findOneReserva(@Param('id') id: number) {
+    return this.adminService.findOne('reservas', id);
+  }
+
+  @Post('reservas')
+  createReserva(@Body() data: any) {
+    return this.adminService.create('reservas', data);
+  }
+
+  @Put('reservas/:id')
+  updateReserva(@Param('id') id: number, @Body() data: any) {
+    return this.adminService.update('reservas', id, data);
+  }
+
+  @Delete('reservas/:id')
+  deleteReserva(@Param('id') id: number) {
+    return this.adminService.remove('reservas', id);
+  }
+
+  // CUADRE
+  @Get('cuadre')
+  findAllCuadre() {
+    return this.adminService.findAll('cuadre');
+  }
+
+  @Get('cuadre/:id')
+  findOneCuadre(@Param('id') id: number) {
+    return this.adminService.findOne('cuadre', id);
+  }
+
+  @Post('cuadre')
+  createCuadre(@Body() data: any) {
+    return this.adminService.create('cuadre', data);
+  }
+
+  @Put('cuadre/:id')
+  updateCuadre(@Param('id') id: number, @Body() data: any) {
+    return this.adminService.update('cuadre', id, data);
+  }
+
+  @Delete('cuadre/:id')
+  deleteCuadre(@Param('id') id: number) {
+    return this.adminService.remove('cuadre', id);
+  }
+}
