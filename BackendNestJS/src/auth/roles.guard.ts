8f2b12fd13cbbba00e1e6ae66b2515b90f577f1b
Daniel Hurtado <dhurtado9611@gmail.com>
2025-04-08 08:12:44 -0500

    Refactor RolesGuard: mejorar la legibilidad del código al reestructurar importaciones y simplificar la lógica de autorización

--- BackendNestJS/src/auth/roles.guard.ts
+++ BackendNestJS/src/auth/roles.guard.ts
@@ -1,17 +1,31 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext): boolean {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
-      context.getHandler(), context.getClass()
+      context.getHandler(),
+      context.getClass(),
     ]);
-    if (!requiredRoles) return true;
+    if (!requiredRoles) {
+      return true;
+    }
 
-    const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.includes(user?.role);
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    return requiredRoles.some((role) =>
+      user.rol?.toLowerCase().trim() === role.toLowerCase().trim()
+    );
   }
 }
