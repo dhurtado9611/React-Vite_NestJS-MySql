502cc283d7b55f3f2f5a571f6ce05b45fb2e79c7
Daniel Hurtado <dhurtado9611@gmail.com>
2025-04-13 01:12:23 -0500

    Refactor CuadreController: agregar guardias de roles para controlar el acceso a la creación de Cuadre

--- BackendNestJS/src/cuadrecaja/cuadre.controller.ts
+++ BackendNestJS/src/cuadrecaja/cuadre.controller.ts
@@ -13,13 +13,16 @@ import { CuadreService } from './cuadre.service';
 import { CreateCuadreDto } from './dto/create-cuadre.dto';
 import { UpdateCuadreDto } from './dto/update-cuadre.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles.decorator';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('cuadre')
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class CuadreController {
   constructor(private readonly cuadreService: CuadreService) {}
 
   @Post()
+  @Roles('admin', 'invitado')
   create(@Body() createCuadreDto: CreateCuadreDto) {
     return this.cuadreService.create(createCuadreDto);
   }
