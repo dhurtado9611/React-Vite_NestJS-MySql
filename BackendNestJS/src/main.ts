c6bb7e277866340ee5878b81477e07f01093af7b
Daniel Hurtado <dhurtado9611@gmail.com>
2025-04-13 00:45:58 -0500

    Refactor main.ts: agregar validación global de DTOs y mejorar la configuración de CORS

--- BackendNestJS/src/main.ts
+++ BackendNestJS/src/main.ts
@@ -1,23 +1,26 @@
-// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // ✅ Validación global de DTOs
+  app.useGlobalPipes(new ValidationPipe());
+
+  // ✅ Configuración de CORS
   app.enableCors({
     origin: [
       'https://react-vite-nest-js-my-ds74mxdem-danielhurtados-projects.vercel.app',
-      'https://react-vite-nest-js-my-sql.vercel.app',// 👈 Agrega esta nueva URL también
-      'https://elesconditemotel.lat'
+      'https://react-vite-nest-js-my-sql.vercel.app',
+      'https://elesconditemotel.lat',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
-  
+
   const PORT = process.env.PORT || 3000;
-  // IMPORTANTE: Añadir explícitamente '0.0.0.0' para escuchar en todas las interfaces
   await app.listen(PORT, '0.0.0.0');
   console.log(`Aplicación corriendo en el puerto: ${PORT}`);
 }
