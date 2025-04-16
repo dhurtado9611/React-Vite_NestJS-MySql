b47b394956429ffb79d1aa10ecfb392be9069828
Daniel Mauricio Hurtado Vallejo <dhurtado9611@gmail.com>
2025-04-15 23:03:12 -0500

    Refactor Home component: update wrapper element for improved layout structure

--- FrontendViteReact/src/pages/Home.tsx
+++ FrontendViteReact/src/pages/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
   }, [location]);
 
   return (
-    <>
+    <div className="ml-0 flex-1 p-0">
       <div
         className="ml-0 p-0 w-full min-h-screen px-4 sm:px-6 lg:px-8 pt-16 pb-20 text-white bg-cover bg-center flex flex-col items-center justify-center text-center gap-6 relative"
         style={{ backgroundImage: `url(${background})` }}
@@ -40,9 +40,8 @@ const Home = () => {
           <Carousel setBackground={setBackground} />
         </div>
       </div>
-
       <Footer />
-    </>
+    </div>
   );
 };
 
