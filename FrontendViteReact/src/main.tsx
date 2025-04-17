60028c28e265deca912a380c319d4b467f94faf5
Daniel Mauricio Hurtado Vallejo <dhurtado9611@gmail.com>
2025-04-16 21:49:28 -0500

    Fix import paths in ReservasForm and update Bootstrap import comment for clarity

--- FrontendViteReact/src/main.tsx
+++ FrontendViteReact/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css' // ✅ Importar Bootstrap correctamente
+import 'bootstrap/dist/css/bootstrap.min.css' // Importar Bootstrap correctamente
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
