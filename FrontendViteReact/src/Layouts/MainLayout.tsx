161b3fbb2accdc1d325ca96c2890facae1d776ce
Daniel Mauricio Hurtado Vallejo <dhurtado9611@gmail.com>
2025-04-18 10:23:27 -0500

    Update MainLayout to allow overflow scrolling for better content accessibility

--- FrontendViteReact/src/Layouts/MainLayout.tsx
+++ FrontendViteReact/src/Layouts/MainLayout.tsx
@@ -4,9 +4,9 @@ import NavbarTop from '../components/Navbar';
 
 const MainLayout = () => {
   return (
-    <div className="flex min-h-screen w-full bg-white overflow-hidden">
+    <div className="flex min-h-screen w-full bg-white overflow-auto">
       <Sidebar />
-      <div className="flex-1 px-0 sm:px-6 lg:px-8">
+      <div className="flex-1 px-0 sm:px-6 lg:px-8 overflow-auto">
         <Outlet />
       </div>
     </div>
