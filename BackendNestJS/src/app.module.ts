0fffdb73a0275a5dd0bde226c9b1e5c5054205f8
Daniel Hurtado <dhurtado9611@gmail.com>
2025-05-22 23:59:20 -0500

    Refactor app.module.ts: reorganizar importaciones y eliminar entidad PreciosInventario de la configuración de TypeOrm

--- BackendNestJS/src/app.module.ts
+++ BackendNestJS/src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+
 import { Reserva } from './reservas/reserva.entity';
 import { User } from './auth/user.entity';
+import { Cuadre } from './cuadrecaja/cuadre.entity';
+import { Inventario } from './inventario/inventario.entity';
+
 import { ReservaService } from './reservas/reserva.service';
 import { ReservaController } from './reservas/reserva.controller';
+
 import { AuthModule } from './auth/auth.module';
 import { CuadreModule } from './cuadrecaja/cuadre.module';
-import { Cuadre } from './cuadrecaja/cuadre.entity';
 import { AdminModule } from './admin/admin.module';
 import { ReservasClienteModule } from './cliente/reservas_cliente.module';
 import { InventarioModule } from './inventario/inventario.module';
-import { Inventario } from './inventario/inventario.entity';
-import { PreciosInventario } from './preciosInventario/precios-inventario.entity'; 
 import { PreciosInventarioModule } from './preciosInventario/precios-inventario.module';
 
 
@@ -26,10 +28,11 @@ import { PreciosInventarioModule } from './preciosInventario/precios-inventario.
     TypeOrmModule.forRoot({
       type: 'mysql',
       url: process.env.MYSQL_URL, 
-      entities: [Reserva, User, Cuadre, Inventario, PreciosInventario], 
+      entities: [Reserva, User, Cuadre, Inventario], 
       synchronize: true, 
     }),
     CuadreModule,
+    TypeOrmModule.forFeature([Reserva]),
     AuthModule,
   ],
   controllers: [ReservaController],
