f31b34220fa1531800065d26cfd708960c04cb8f
Daniel Hurtado <dhurtado9611@gmail.com>
2026-02-05 00:21:39 -0500

    @Entity('preciosInventario')

--- BackendNestJS/src/preciosInventario/precios-inventario.entity.ts
+++ BackendNestJS/src/preciosInventario/precios-inventario.entity.ts
@@ -1,11 +1,13 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 
-@Entity('preciosInventario')
+// Asegúrate de mantener el nombre de la tabla si lo agregaste antes, si no, déjalo @Entity()
+@Entity('preciosInventario') 
 export class PreciosInventario {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true })
+  // CORRECCIÓN: Quitamos { unique: true } para evitar el crash por nombres vacíos duplicados
+  @Column() 
   nombre: string;
 
   @Column('decimal', { precision: 10, scale: 2 })
